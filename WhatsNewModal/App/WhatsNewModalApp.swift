8f4c8c22f13ad5911864d082fc264feffb2011c3
ViktorNikolaevi4 <v87v87@icloud.com>
2025-09-30 17:12:43 +0300

    padding

--- WhatsNewModal/App/WhatsNewModalApp.swift
+++ WhatsNewModal/App/WhatsNewModalApp.swift
@@ -25,4 +25,3 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
         }
     }
 }
-
--- WhatsNewModal/View/WhatsNewModalView.swift
+++ WhatsNewModal/View/WhatsNewModalView.swift
@@ -76,7 +76,7 @@ struct WhatsNewModalView: View {
             .buttonStyle(PrimaryCapsuleButtonStyle())
             .padding(.bottom, 4)
         }
-        .padding(48)
+        .padding(46)
         .frame(minWidth: 420, minHeight: 520)
      //   .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
         .background(drawBackground ? Color(nsColor: .windowBackgroundColor) : .clear)
